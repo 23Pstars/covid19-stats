ba5899b753d0b3a421493a84f91b8decc261feb0
Ahmad Zafrullah <23Pstars@gmail.com>
2021-04-20 12:46:03 +0800

    updates

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { merge } = require('webpack-merge');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
